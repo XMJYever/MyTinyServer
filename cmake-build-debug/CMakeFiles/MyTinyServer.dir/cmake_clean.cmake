26840a9d345059cd79fab805b6c25a6e6de67b4f
XMJYever <liuxy11@mail.ustc.edu.cn>
2021-01-10 20:33:20 +0800

    create a repo

---
+++ cmake-build-debug/CMakeFiles/MyTinyServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyTinyServer.dir/main.cpp.o"
+  "MyTinyServer"
+  "MyTinyServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyTinyServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ locker.h
@@ -0,0 +1,10 @@
+//
+// Created by xmjyever on 2021/1/10.
+//
+
+#ifndef MYTINYSERVER_LOCKER_H
+#define MYTINYSERVER_LOCKER_H
+
+
+
+#endif //MYTINYSERVER_LOCKER_H
